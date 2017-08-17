3c101c8484c60a71bd5f007843ee4bd431c8f501
eezhal92 <arkinthesky.69@gmail.com>
2017-08-17 12:57:17 +0800

    add macro for test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,10 +4,10 @@ namespace Tests;
 
 use Exception;
 use App\Exceptions\Handler;
-use Illuminate\Foundation\Testing\TestResponse;
-use Illuminate\Support\Collection;
-use Illuminate\Contracts\Debug\ExceptionHandler;
 use PHPUnit\Framework\Assert;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Foundation\Testing\TestResponse;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
@@ -34,12 +34,25 @@ abstract class TestCase extends BaseTestCase
             return $this->original->getData()[$key];
         });
 
-        Collection::macro('assertContains', function ($item) {
-            Assert::assertTrue($this->contains($item));
+        TestResponse::macro('assertViewIs', function ($name) {
+            Assert::assertEquals($name, $this->original->name());
         });
 
-        Collection::macro('assertNotContains', function ($item) {
-            Assert::assertFalse($this->contains($item));
+        EloquentCollection::macro('assertContains', function ($value) {
+            Assert::assertTrue($this->contains($value), "Failed asserting that the collection contains the specified value.");
+        });
+
+        EloquentCollection::macro('assertNotContains', function ($value) {
+            Assert::assertFalse($this->contains($value), "Failed asserting that the collection does not contain the specified value.");
+        });
+
+        EloquentCollection::macro('assertEquals', function ($items) {
+            Assert::assertEquals(count($this), count($items));
+
+            $this->zip($items)->each(function ($pair) {
+                list($a, $b) = $pair;
+                Assert::assertTrue($a->is($b));
+            });
         });
     }
 
