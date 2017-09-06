546f0dfc058dc51db25598a1ad75ff1c9fe2791b
eezhal92 <arkinthesky.69@gmail.com>
2017-09-06 14:34:14 +0800

    add google analytics

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -14,5 +14,15 @@
 
     <script src="{{ mix('/js/manifest.js') }}"></script>
     <script src="{{ mix('/js/landing.js') }}"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-105977759-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
   </body>
 </html>
